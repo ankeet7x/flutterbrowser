d52d9489fccdf5a94cbb2fb3071331f10e115230
ankeet7x <ankeetkarki30@gmail.com>
2020-09-04 17:32:35 +0545

    Webview scaffold added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +7,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController textController = TextEditingController();
+  String urlValue = "https://google.com";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return WebviewScaffold(
+      url: urlValue,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Container(
+          width: size.width / 1.2,
+          height: 45,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.white),
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: TextField(
+                    controller: textController,
+                    decoration: InputDecoration(hintText: "Enter url"),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.blue,
+                ),
+                onPressed: () async {
+                  setState(() {
+                    urlValue = textController.text;
+                  });
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
